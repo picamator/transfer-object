6cace6dd01bc23da57f461e4c7c669ff1ab2813c
Sergii Pryz <picamator@gmail.com>
2025-04-17 22:04:30 +0200

    Fixed dock-block typos

--- src/TransferGenerator/Generator/Render/Template/Template.tpl.php
+++ src/TransferGenerator/Generator/Render/Template/Template.tpl.php
@@ -19,7 +19,7 @@ namespace $templateTransfer->classNamespace;
 /**
  * Specification:
  * - Class is automatically generated based on a definition file.
- * - To modify it, please update the corresponding definition file and run generator again.
+ * - To modify it, please update the corresponding definition file and run the generator again.
  *
  * Note: Do not manually edit this file, as changes will be overwritten.
  */
--- tests/integration/Transfer/Generated/ItemTransfer.php
+++ tests/integration/Transfer/Generated/ItemTransfer.php
@@ -15,7 +15,7 @@ use Picamator\TransferObject\Transfer\TransferTrait;
 /**
  * Specification:
  * - Class is automatically generated based on a definition file.
- * - To modify it, please update the corresponding definition file and run generator again.
+ * - To modify it, please update the corresponding definition file and run the generator again.
  *
  * Note: Do not manually edit this file, as changes will be overwritten.
  */
