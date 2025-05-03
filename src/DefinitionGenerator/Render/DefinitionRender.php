f9beb6b272c1f067b6c5d247c95f666e630be689
Sergii Pryz <picamator@gmail.com>
2025-05-03 19:14:04 +0200

    Added schema to the generated definition files

--- src/DefinitionGenerator/Render/DefinitionRender.php
+++ src/DefinitionGenerator/Render/DefinitionRender.php
@@ -10,6 +10,12 @@ use Picamator\TransferObject\Generated\DefinitionPropertyTransfer;
 
 readonly class DefinitionRender implements DefinitionRenderInterface
 {
+    private const string SCHEMA = <<<'START'
+# $schema: https://raw.githubusercontent.com/picamator/transfer-object/refs/heads/main/schema/definition.schema.json
+
+
+START;
+
     private const string CLASS_TEMPLATE = <<<'START'
 # %s
 %s:
@@ -30,6 +36,11 @@ START;
   %s:
     dateTimeType: %s
 START;
+
+    public function renderSchema(): string
+    {
+        return self::SCHEMA;
+    }
 
     public function renderDefinitionContent(DefinitionContentTransfer $contentTransfer): string
     {
