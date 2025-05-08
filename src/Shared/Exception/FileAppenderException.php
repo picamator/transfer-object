e00334d45848b3161da14e64db4e5167dea9da05
Sergii Pryz <picamator@gmail.com>
2025-05-08 22:00:57 +0200

    Rephrased composer suggests, removed unused imports

--- src/Shared/Exception/FileAppenderException.php
+++ src/Shared/Exception/FileAppenderException.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Picamator\TransferObject\Shared\Exception;
 
 use Exception;
-use Picamator\TransferObject\Shared\Exception\TransferExceptionInterface;
 
 class FileAppenderException extends Exception implements TransferExceptionInterface
 {
