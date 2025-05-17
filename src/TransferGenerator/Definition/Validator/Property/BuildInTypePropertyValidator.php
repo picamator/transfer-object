6641f1dcd144c6e460495fdfe2306be9c58e71f6
Sergii Pryz <picamator@gmail.com>
2025-05-17 14:34:31 +0200

    Cached BuildInTypePropertyValidator result

--- src/TransferGenerator/Definition/Validator/Property/BuildInTypePropertyValidator.php
+++ src/TransferGenerator/Definition/Validator/Property/BuildInTypePropertyValidator.php
@@ -8,11 +8,16 @@ use Picamator\TransferObject\Generated\DefinitionPropertyTransfer;
 use Picamator\TransferObject\Generated\ValidatorMessageTransfer;
 use Picamator\TransferObject\Shared\Validator\ValidatorMessageTrait;
 
-readonly class BuildInTypePropertyValidator implements PropertyValidatorInterface
+class BuildInTypePropertyValidator implements PropertyValidatorInterface
 {
     use ValidatorMessageTrait;
 
     private const string UNSUPPORTED_TYPE_ERROR_MESSAGE_TEMPLATE = 'Property "%s" with type "%s" is not supported.';
+
+    /**
+     * @var array<string,\Picamator\TransferObject\Generated\ValidatorMessageTransfer>
+     */
+    private static array $validatorCache;
 
     public function isApplicable(DefinitionPropertyTransfer $propertyTransfer): bool
     {
@@ -21,13 +26,21 @@ readonly class BuildInTypePropertyValidator implements PropertyValidatorInterfac
 
     public function validate(DefinitionPropertyTransfer $propertyTransfer): ValidatorMessageTransfer
     {
-        if ($propertyTransfer->buildInType?->isAllowed()) {
-            return $this->createSuccessMessageTransfer();
+        /** @var \Picamator\TransferObject\TransferGenerator\Definition\Enum\BuildInTypeEnum $buildInType */
+        $buildInType = $propertyTransfer->buildInType;
+        $cacheKey = $buildInType->value;
+
+        if (isset(self::$validatorCache[$cacheKey])) {
+            return self::$validatorCache[$cacheKey];
+        }
+
+        if ($buildInType->isAllowed()) {
+            return self::$validatorCache[$cacheKey] = $this->createSuccessMessageTransfer();
         }
 
         $errorMessage = $this->getErrorMessage($propertyTransfer);
 
-        return $this->createErrorMessageTransfer($errorMessage);
+        return self::$validatorCache[$cacheKey] = $this->createErrorMessageTransfer($errorMessage);
     }
 
     private function getErrorMessage(DefinitionPropertyTransfer $propertyTransfer): string
