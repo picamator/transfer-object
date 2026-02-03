a997d2ffb16860492dd2866e06959c1f2bc116b2
Sergii Pryz <picamator@gmail.com>
2026-02-03 20:42:20 +0100

    Microoptimized php functions to use global namespace (data assert trait)

--- src/Transfer/Attribute/Transformer/DataAssertTrait.php
+++ src/Transfer/Attribute/Transformer/DataAssertTrait.php
@@ -13,12 +13,12 @@ trait DataAssertTrait
      */
     final protected function assertArray(mixed $data): void
     {
-        if (is_array($data)) {
+        if (\is_array($data)) {
             return;
         }
 
         throw new DataAssertTransferException(
-            sprintf(
+            \sprintf(
                 'Data must be of type array, "%s" given.',
                 get_debug_type($data),
             ),
@@ -31,10 +31,10 @@ trait DataAssertTrait
     final protected function assertInvalidType(mixed $data, string $expectedType): never
     {
         throw new DataAssertTransferException(
-            sprintf(
+            \sprintf(
                 'Data must be of type %s, "%s" given.',
                 $expectedType,
-                get_debug_type($data),
+                \get_debug_type($data),
             ),
         );
     }
@@ -44,14 +44,14 @@ trait DataAssertTrait
      */
     final protected function assertStringOrInt(mixed $data): void
     {
-        if (is_string($data) || is_int($data)) {
+        if (\is_string($data) || \is_int($data)) {
             return;
         }
 
         throw new DataAssertTransferException(
             sprintf(
                 'Data must be of type string or integer, "%s" given.',
-                get_debug_type($data),
+                \get_debug_type($data),
             ),
         );
     }
