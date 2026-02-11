8b7f1c9aa53d1b5b102e43b774ffb3bd7b913d60
Sergii Pryz <picamator@gmail.com>
2026-02-11 21:22:00 +0100

    Covered transfer generator command - debug message line separator

--- tests/integration/Command/TransferGeneratorCommandTest.php
+++ tests/integration/Command/TransferGeneratorCommandTest.php
@@ -65,7 +65,8 @@ final class TransferGeneratorCommandTest extends TestCase
 
         // Assert
         $this->commandTester->assertCommandIsSuccessful();
-        $this->assertStringContainsString('command.transfer.yml: CommandTransfer', $output);
+        $this->assertStringContainsString('command.first.transfer.yml: CommandFirstTransfer', $output);
+        $this->assertStringContainsString('command.second.transfer.yml: CommandSecondTransfer', $output);
         $this->assertStringContainsString('All Transfer Objects were generated successfully!', $output);
     }
 
