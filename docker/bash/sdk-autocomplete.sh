2fc5e6b79007d81d9ec18d158588e0198d3846f5
Sergii Pryz <picamator@gmail.com>
2026-02-21 12:01:15 +0100

    Added docker/sdk install, optimized docker/sdk commands, updated autocomplete

--- docker/bash/sdk-autocomplete.sh
+++ docker/bash/sdk-autocomplete.sh
@@ -8,12 +8,12 @@ _docker_sdk_autocomplete() {
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     # define the main commands for docker/sdk
-    opts="build start stop cli composer phpstan phpunit phpunit-group phpcs phpcbf hook-install hook to-generate to-generate-bulk df-generate"
+    opts="install build start stop cli composer phpstan phpunit phpunit-group phpcs phpcbf hook-install hook to-generate to-generate-bulk df-generate"
 
     case "${prev}" in
         composer)
             # define subcommands for 'composer'
-            COMPREPLY=( $(compgen -W "install update why phpunit phpunit-group phpstan transfer-generate transfer-generate-bulk definition-generate captainhook phpcs phpcbf list" -- "${cur}") )
+            COMPREPLY=( $(compgen -W "install update why test phpunit phpunit-group phpstan transfer-generate transfer-generate-bulk definition-generate captainhook phpcs phpcbf list" -- "${cur}") )
             return 0
             ;;
         hook)
