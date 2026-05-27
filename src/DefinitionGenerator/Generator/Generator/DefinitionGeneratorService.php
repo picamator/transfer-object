6eca0b24ee336539b441d06bd8413482a14ba524
Sergii Pryz <picamator@gmail.com>
2026-05-27 11:32:20 +0200

    Extracted filesystem transfer creation from definition generator service to separate builder class

--- src/DefinitionGenerator/Generator/Generator/DefinitionGeneratorService.php
+++ src/DefinitionGenerator/Generator/Generator/DefinitionGeneratorService.php
@@ -4,20 +4,21 @@ declare(strict_types=1);
 
 namespace Picamator\TransferObject\DefinitionGenerator\Generator\Generator;
 
+use Picamator\TransferObject\DefinitionGenerator\Generator\Generator\Builder\FilesystemBuilderInterface;
 use Picamator\TransferObject\DefinitionGenerator\Generator\Generator\Processor\DefinitionGeneratorProcessorInterface;
-use Picamator\TransferObject\Generated\DefinitionFilesystemTransfer;
 use Picamator\TransferObject\Generated\DefinitionGeneratorTransfer;
 
 readonly class DefinitionGeneratorService implements DefinitionGeneratorServiceInterface
 {
     public function __construct(
+        private FilesystemBuilderInterface $builder,
         private DefinitionGeneratorProcessorInterface $processor,
     ) {
     }
 
     public function generateDefinitionsOrFail(DefinitionGeneratorTransfer $generatorTransfer): int
     {
-        $filesystemTransfer = $this->createFilesystemTransfer($generatorTransfer);
+        $filesystemTransfer = $this->builder->createFilesystemTransfer($generatorTransfer);
 
         try {
             $this->processor->preProcess($filesystemTransfer);
@@ -27,15 +28,5 @@ readonly class DefinitionGeneratorService implements DefinitionGeneratorServiceI
         }
 
         return $result;
-    }
-
-    private function createFilesystemTransfer(
-        DefinitionGeneratorTransfer $generatorTransfer,
-    ): DefinitionFilesystemTransfer {
-        $filesystemTransfer = new DefinitionFilesystemTransfer();
-        $filesystemTransfer->fileName = lcfirst($generatorTransfer->content->className);
-        $filesystemTransfer->definitionPath = $generatorTransfer->definitionPath;
-
-        return $filesystemTransfer;
     }
 }
